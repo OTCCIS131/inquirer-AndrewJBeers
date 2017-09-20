904497a775129356f1d4333b0f447ffbaa98b910
Unknown <ajbeers89@gmail.com>
2017-09-20 12:32:37 -0500

    breakthrough in class - ask for help sooner next time!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var inquirer = require('inquirer');
 const chalk = require('chalk');
 const log = console.log;
-const prompt = inquirer.prompt;
+// const prompt = inquirer.prompt;
 
 log("")
 log("██████╗ ██╗███████╗███████╗ █████╗     ██████╗  ██████╗ ███╗   ██╗ █████╗ ███╗   ██╗███████╗ █████╗ ██╗██╗")
@@ -13,3 +13,54 @@ log("╚═╝     ╚═╝╚══════╝╚══════╝╚�
 log("")
 
 
+
+
+
+
+
+
+
+let questions = [{
+    type: 'list',
+    message:"What size of pizza would you like?",
+    name:"size",
+    choices: ['Small', 'Medium', 'Large'],
+    // filter: function (val){
+    //     return val.toLowerCase();
+    // }
+},{
+    type: 'checkbox',
+    message:"What toppings would you like?",
+    name: 'toppings',
+    choices: ["Peperonni", "Sassage","Extra Cheese","Onions","Peppers"]
+},{
+    type: 'list',
+    message: "How would you like your pizza cut?",
+    name: "cut",
+    choices: ['Triangle', 'Square']
+},{
+    type: 
+    message:
+    name:
+    choices:
+},{
+    type: 
+    message:
+    name:
+    choices:
+},{
+    type: 
+    message:
+    name:
+    choices:
+}]
+
+
+    
+    
+
+    inquirer.prompt(questions).then(function(answers){
+
+        console.log(answers)
+
+    });
